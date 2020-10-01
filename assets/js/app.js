121608aab7fd4be82caf8d1069baea56e64d08cf
David Eisenstein <davidseisenstein@gmail.com>
2020-09-30 19:44:57 -0500

    successfully read data using d3.csv

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+// @TODO: YOUR CODE HERE!
+
+// declare the dimensions of our SVG Canvas and image
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import the data using d3.csv
+d3.csv("/assets/data/data.csv").then(function(data, err) {
+    if (err) throw err;
+
+    console.log(data)
+})
